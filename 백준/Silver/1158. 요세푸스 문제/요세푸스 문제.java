52fa7ccb4a93e3acd274bc9b02406581c2e90dd7
bokyung95 <102026109+bokyung95@users.noreply.github.com>
2022-09-26 15:48:41 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 580 ms, Memory: 294824 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+import java.util.stream.IntStream;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        Queue<Integer> queue = new LinkedList<>();
+        int[] result = new int[n];
+
+        IntStream.range(1, n + 1).forEach(queue::add); //반복문 대신
+
+        int idx = 0;
+        while (!queue.isEmpty()) {
+            for (int i = 0; i < k - 1; i++) {
+                queue.add(queue.poll());
+            }
+//            System.out.println(queue.peek());
+            result[idx] = queue.poll();
+            idx++;
+        }
+//        System.out.println(Arrays.toString(result));
+//        System.out.print(result + " ");
+        StringBuilder sb = new StringBuilder();
+        sb.append("<");
+
+        for (int i = 0; i < result.length; i++) {
+            if (i == 0) {
+                sb.append(String.valueOf(result[i]));
+            } else if (i < result.length) {
+                sb.append(", " + String.valueOf(result[i]));
+            }
+
+        }
+        sb.append(">");
+        System.out.println(sb.toString());
+
+    }
+}
