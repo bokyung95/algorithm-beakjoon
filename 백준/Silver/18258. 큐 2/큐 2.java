20000578ad289166f004be9837aec3005a941e49
bokyung95 <102026109+bokyung95@users.noreply.github.com>
2022-12-05 15:30:27 +0900

    [Silver IV] Title: 큐 2, Time: 1400 ms, Memory: 350600 KB -BaekjoonHub

--- 백준/Silver/18258. 큐 2/큐 2.java
+++ 백준/Silver/18258. 큐 2/큐 2.java
@@ -1,49 +1,53 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        LinkedList<Integer> q = new LinkedList<>();
-        int num = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        Queue<Integer> queue = new LinkedList<>();
+        int last = 0;
 
-        for(int i=0; i<num; i++) {
-            String[] op = br.readLine().split(" ");
-            switch(op[0]) {
-                case "push":
-                    q.offer(Integer.parseInt(op[1]));
-                    break;
-                case "pop":
-                    if(q.isEmpty())
-                        sb.append(-1).append("\n");
-                    else
-                        sb.append(q.poll()).append("\n");
-                    break;
-                case "front":
-                    if(q.isEmpty())
-                        sb.append(-1).append("\n");
-                    else
-                        sb.append(q.peek()).append("\n");
-                    break;
-                case "back":
-                    if(q.isEmpty())
-                        sb.append(-1).append("\n");
-                    else
-                        sb.append(q.peekLast()).append("\n");
-                    break;
-                case "size":
-                    sb.append(q.size()).append("\n");
-                    break;
-                case "empty":
-                    if(q.isEmpty())
-                        sb.append(1).append("\n");
-                    else
-                        sb.append(0).append("\n");
-                    break;
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            String command = st.nextToken();
+
+            if ("push".equals(command)) {
+                last = Integer.parseInt(st.nextToken());
+                queue.add(last);
+            } else if ("pop".equals(command)) {
+                if (queue.isEmpty()) {
+                    sb.append(-1).append("\n");
+                } else {
+                    sb.append(queue.poll()).append("\n");
+                }
+            } else if ("size".equals(command)) {
+                sb.append(queue.size()).append("\n");
+            } else if ("empty".equals(command)) {
+                if (queue.isEmpty()) {
+                    sb.append(1).append("\n");
+                } else {
+                    sb.append(0).append("\n");
+                }
+            } else if ("front".equals(command)) {
+                if (queue.isEmpty()) {
+                    sb.append(-1).append("\n");
+                } else {
+                    sb.append(queue.peek()).append("\n");
+                }
+            } else {
+                if (queue.isEmpty()) {
+                    sb.append(-1).append("\n");
+                } else {
+                    sb.append(Integer.toString(last)).append("\n");
+                }
             }
         }
         System.out.println(sb);
