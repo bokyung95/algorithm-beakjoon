e93720e339342ad684f9a8a7c490af28cadfae43
bokyung95 <102026109+bokyung95@users.noreply.github.com>
2022-12-05 14:53:45 +0900

    [Silver IV] Title: 괄호, Time: 128 ms, Memory: 14284 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,35 +1,36 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Stack;
+import java.util.*;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine());
-
-        Stack<Character> stack = new Stack<>();
+        StringBuilder sb = new StringBuilder();
 
         for (int i = 0; i < t; i++) {
-            String str = br.readLine();
-            for (int j = 0; j < str.length(); j++) {
-                if ('(' == str.charAt(j)) {
-                    stack.push('(');
+            Stack<Character> stack = new Stack<>();
+            char[] arr = br.readLine().toCharArray();
+            boolean isVps = true;
+            for (int j = 0; j < arr.length; j++) {
+                if (arr[j] == '(') {
+                    stack.push(arr[j]);
                 } else {
                     if (stack.isEmpty()) {
-                        stack.push(')');
+                        sb.append("NO").append("\n");
+                        isVps = false;
                         break;
                     } else {
                         stack.pop();
                     }
                 }
             }
-            if (stack.isEmpty()) {
-                System.out.println("YES");
-            } else {
-                System.out.println("NO");
+            if (isVps && stack.isEmpty()) {
+                sb.append("YES").append("\n");
+            } else if (!stack.isEmpty()) {
+                sb.append("NO").append("\n");
             }
-            stack.clear();
         }
+        System.out.println(sb);
     }
 }
