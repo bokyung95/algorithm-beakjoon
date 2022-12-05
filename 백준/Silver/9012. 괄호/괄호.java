fd604abcd9060c5d2657b2c65174140cb3b36160
bokyung95 <102026109+bokyung95@users.noreply.github.com>
2022-12-05 15:13:32 +0900

    [Silver IV] Title: 괄호, Time: 128 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,36 +1,41 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
-        StringBuilder sb = new StringBuilder();
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int t;
+    static StringBuilder sb = new StringBuilder();
+    static int cnt = 0;
+
+    public static void main(String[] args) throws IOException {
+        t = Integer.parseInt(br.readLine());
 
         for (int i = 0; i < t; i++) {
-            Stack<Character> stack = new Stack<>();
-            char[] arr = br.readLine().toCharArray();
-            boolean isVps = true;
-            for (int j = 0; j < arr.length; j++) {
-                if (arr[j] == '(') {
-                    stack.push(arr[j]);
-                } else {
-                    if (stack.isEmpty()) {
-                        sb.append("NO").append("\n");
-                        isVps = false;
-                        break;
-                    } else {
-                        stack.pop();
-                    }
-                }
-            }
-            if (isVps && stack.isEmpty()) {
-                sb.append("YES").append("\n");
-            } else if (!stack.isEmpty()) {
-                sb.append("NO").append("\n");
-            }
+            cnt = 0;
+            String str = br.readLine();
+            sb.append(isVps(str)).append("\n");
         }
         System.out.println(sb);
+    }
+
+    public static String isVps(String str) {
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '(') {
+                cnt++;
+            } else {
+                if (cnt == 0) {
+                    return "NO";
+                } else {
+                    cnt--;
+                }
+            }
+        }
+        if (cnt > 0) {
+            return "NO";
+        } else {
+            return "YES";
+        }
     }
 }
