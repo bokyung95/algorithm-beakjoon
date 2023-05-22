e1241e63582f0e5597ac63e5b39c7b5bb92800fb
bokyung95 <102026109+bokyung95@users.noreply.github.com>
2023-05-22 17:52:28 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 556 ms, Memory: 51848 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -4,30 +4,28 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int[] arr = new int[n + 1];
-        int[] sum = new int[n + 1];
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
 
-        st = new StringTokenizer(br.readLine());
-        for (int i = 1; i < n + 1; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
+		int[] sum = new int[n + 1];
+		st = new StringTokenizer(br.readLine());
 
-        for (int i = 1; i < n + 1; i++) {
-            sum[i] = sum[i - 1] + arr[i];
-        }
+		for (int i = 1; i < n + 1; i++) {
+			sum[i] = sum[i - 1] + Integer.parseInt(st.nextToken());
+		}
 
-        for (int i = 0; i < m; i++) {
-            st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-            System.out.println(sum[end] - sum[start - 1]);
-        }
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < m; i++) {
+			st = new StringTokenizer(br.readLine());
+			int start = Integer.parseInt(st.nextToken());
+			int end = Integer.parseInt(st.nextToken());
+			sb.append(sum[end] - sum[start - 1]).append("\n");
+		}
 
-    }
+		System.out.println(sb.toString());
+	}
 }
